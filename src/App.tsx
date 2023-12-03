4a27fd10b75096a829355ba18d5802916a69f7ea
nanoubon <programmer_ubon@hotmail.com>
2023-12-03 12:23:02 +0700

    insert jobs

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import "./App.css";
 import "./index.css";
 import Avatar from "./components/Avatar";
 import Personal from "./components/Personal";
+import Timeline from "./components/Timeline";
 
 function App() {
   return (
     <>
-    <div className="gradient_bg">
-      <div className=" container">
-      <div> <Avatar/></div>
-      <div> <Personal/></div>
-      </div>
+      <div className="gradient_bg">
+        <div className=" container">
+          <div>
+            
+            <Avatar />
+          </div>
+          <div>
+            
+            <Personal />
+          </div>
+          <div>
+            
+            <Timeline />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,62 +1,88 @@
-
-// import { Timeline } from 'flowbite-react';
-
 const jobs_name = [
     {
-        name: 'Stream I.T. Consulting Ltd., Bangkok Province.',
-        jobdetail: 'Installed and performed maintenance on domain controller, active directory,' +
-            'finger scan and Internet server by mail server SMTP, POP3 and CCTV.' +
-            'Installed and Designed LAN, WIFI network.' +
-            'Rolled out Desktop PCs and Laptop as needed.' +
-            'Responsible for determining software and hardware resolutions' +
-            'Work as primary support analyst for company.',
-        posstion :'Software Engineer',
-        year : '2023 – Present',
+        name: "Stream I.T. Consulting Ltd., Bangkok Province.",
+        jobdetail:
+            "Installed and performed maintenance on domain controller, active directory," +
+            "finger scan and Internet server by mail server SMTP, POP3 and CCTV." +
+            "Installed and Designed LAN, WIFI network." +
+            "Rolled out Desktop PCs and Laptop as needed." +
+            "Responsible for determining software and hardware resolutions" +
+            "Work as primary support analyst for company.",
+        posstion: "Software Engineer",
+        year: "2023 – Present",
     },
     {
-        name: 'Thai Rotary Engineering Public Company Limited, Rayong Province.',
-        jobdetail: 'Dedicated, reliable IT project leader with excellent interpersonal ' +
-            'skills who builds and maintain strong alliance to motivate teams and drive projects' +
-            ' to successful completion. Strong communication abilities with extensive experience ' +
-            'writing reports crucial to project safety and operations. Experience working with ' +
-            'a variety of technical equipment with the in-depth knowledge required to troubleshoot,' +
-            ' problem-solve and maximize productivity. Reputation as a resourceful team player' +
-            ' working with integrity and professionalism to earn respect, inspire cooperation ' +
-            'and exceed business expectations.',
-        posstion :'Network Engineer and Acting IT Manager ',
-        year : '2015 – 2022',
+        name: "Thai Rotary Engineering Public Company Limited, Rayong Province.",
+        jobdetail:
+            "Dedicated, reliable IT project leader with excellent interpersonal " +
+            "skills who builds and maintain strong alliance to motivate teams and drive projects" +
+            " to successful completion. Strong communication abilities with extensive experience " +
+            "writing reports crucial to project safety and operations. Experience working with " +
+            "a variety of technical equipment with the in-depth knowledge required to troubleshoot," +
+            " problem-solve and maximize productivity. Reputation as a resourceful team player" +
+            " working with integrity and professionalism to earn respect, inspire cooperation " +
+            "and exceed business expectations.",
+        posstion: "Network Engineer and Acting IT Manager ",
+        year: "2015 – 2022",
     },
     {
-        name: 'GOLDEN CITY RAYONG HOTEL, Rayong Province.',
-        jobdetail: 'Installed and performed maintenance on domain controller, active directory,' +
-                    'finger scan and Internet server by mail server SMTP, POP3 and CCTV.' +
-                    'Installed and Designed LAN, WIFI network.' +
-                    'Rolled out Desktop PCs and Laptop as needed.' +
-                    'Responsible for determining software and hardware resolutions' +
-                    'Work as primary support analyst for company.',
-        posstion :'IT Support',
-        year : '2015',
-    }
+        name: "GOLDEN CITY RAYONG HOTEL, Rayong Province.",
+        jobdetail:
+            "Installed and performed maintenance on domain controller, active directory," +
+            "finger scan and Internet server by mail server SMTP, POP3 and CCTV." +
+            "Installed and Designed LAN, WIFI network." +
+            "Rolled out Desktop PCs and Laptop as needed." +
+            "Responsible for determining software and hardware resolutions" +
+            "Work as primary support analyst for company.",
+        posstion: "IT Support",
+        year: "2015",
+    },
 ];
 
-function TimeLine() {
-    const timelineItems = [];
+function Timeline() {
+    return (
+        <div>
+            <div className="p-0">
+                <ol className="relative border-l border-gray-200 dark:border-gray-700">
+                    {jobs_name.map((job,index) =>(
+                    <li key={index} className="mb-10 ml-4">
+                        <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                            {job.year}
+                        </time>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                            {job.name}
+                            <br/>
+                            {job.posstion}
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            {job.jobdetail}
+                        </p>
+                        <a
+                            href="#"
+                            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        >
+                            Learn more{" "}
+                            <svg
+                                className="w-3 h-3 ml-2"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </a>
+                    </li>
+                    ))}
+                </ol>
 
-    for (let i = 0; i < jobs_name.length; i++) {
-        const job = jobs_name[i];
-        timelineItems.push(
-            <Timeline.Item key={i}>
-                <Timeline.Point />
-                <Timeline.Content>
-                    <Timeline.Time className={'text-amber-50'}>{job.year}</Timeline.Time>
-                    <Timeline.Title>{job.name}</Timeline.Title>
-                    <Timeline.Body className={'text-amber-50'}>{job.jobdetail}</Timeline.Body>
-                </Timeline.Content>
-            </Timeline.Item>
-        );
-    }
-
-    return <Timeline>{timelineItems}</Timeline>;
+            </div>
+        </div>
+    );
 }
 
-export default TimeLine;
+export default Timeline;
